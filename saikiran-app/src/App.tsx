7166464d523ac91f9d46a7eedb4004064e573d3b
Saikiran Goud <saikirangoud@saikirans-Air.local>
2024-12-20 15:42:33 +0530

    first commit

---
+++ saikiran-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+import Home from './components/Home'
+import Movies from './components/Movies'
+import Photos from './components/Photos'
+
+export default function App() {
+  return (
+    <div>
+    
+     <Home/>
+    <Photos/>
+    </div>
+  )
+}
